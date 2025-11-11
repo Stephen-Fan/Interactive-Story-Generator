79eccc77d2016dc242986843009747137d80a86b
Qinghong Fan <fanqinghong13@gmail.com>
2025-11-10 22:24:32 -0600

    Update story_generator.py

--- backend/core/story_generator.py
+++ backend/core/story_generator.py
@@ -18,15 +18,6 @@ class StoryGenerator:
 
     @classmethod
     def _get_llm(cls):
-        openai_api_key = os.getenv("CHOREO_OPENAI_CONNECTION_OPENAI_API_KEY")
-        serviceurl = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
-
-        consumerkey = os.getenv("CHOREO_OPENAI_CONNECTION_CONSUMERKEY")
-        consumersecret = os.getenv("CHOREO_OPENAI_CONNECTION_CONSUMERSECRET")
-        tokenurl = os.getenv("CHOREO_OPENAI_CONNECTION_TOKENURL")
-
-        if openai_api_key and serviceurl:
-            return ChatOpenAI(model="gpt-4o-mini", api_key=openai_api_key, base_url=serviceurl)
 
         return ChatOpenAI(model="gpt-4o-mini")
 
